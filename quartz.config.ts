b61969bae5ff44128f32378b1953ff05ada24021
Alex Renkis <arenkis@proton.me>
2025-01-30 14:00:03 -0600

    Auto-sync from Obsidian Public to Quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "simonfidel.github.io",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
